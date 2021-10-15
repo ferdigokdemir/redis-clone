cc26ddca34282940f3ba009e656650dee6102ebe
Ferdi <gokdemirferdi@gmail.com>
2021-10-15 17:13:34 +0300

    apidoc conf

--- handlers.go
+++ handlers.go
@@ -13,30 +13,70 @@ type Item struct {
 	Value string `json:"value"`
 }
 
+/**
+ * @api {post} /getItem Request Item by Key
+ * @apiName GetItem
+ * @apiGroup Item
+ *
+ * @apiBody {String} key Key of the item to get
+ *
+ * @apiSuccess {String} key Key of the item
+ * @apiSuccess {String} value Value of the item
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "key": "name",
+ *       "value": "Ferdi Gökdemir"
+ *     }
+ *
+ * @apiError KeyNotFound Key not found
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "error": "KeyNotFound"
+ *     }
+ */
+
 func GetHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type,access-control-allow-origin, access-control-allow-headers")
+	LogRequest(r)
 	var item Item
 	json.NewDecoder(r.Body).Decode(&item)
-	w.Write([]byte(data[item.Key]))
-	LogRequest(r)
+	if value, ok := data[item.Key]; ok {
+		item.Value = value
+		SuccessResponse(w, item)
+	} else {
+		ErrorResponse(w, "KeyNotFound")
+	}
 }
 
 func SetHandler(w http.ResponseWriter, r *http.Request) {
+	LogRequest(r)
 	var item Item
 	json.NewDecoder(r.Body).Decode(&item)
 	data[item.Key] = item.Value
-	LogRequest(r)
+	SuccessResponse(w, nil)
 }
 
 func FlushHandler(w http.ResponseWriter, r *http.Request) {
-	data = make(map[string]string)
-	os.Remove("db.json")
 	LogRequest(r)
+	data = make(map[string]string)
+	err := os.Remove("db.json")
+	if err != nil {
+		ErrorResponse(w, err.Error())
+	} else {
+		SuccessResponse(w, nil)
+	}
 }
 
 func StartHttpRouter() {
-	http.HandleFunc("/get", GetHandler)
-	http.HandleFunc("/set", SetHandler)
-	http.HandleFunc("/flush", FlushHandler)
+	http.HandleFunc("/api/v1/getItem", GetHandler)
+	http.HandleFunc("/api/v1/setItem", SetHandler)
+	http.HandleFunc("/api/v1/flushDatabase", FlushHandler)
 	if err := http.ListenAndServe(":"+os.Getenv("PORT"), nil); err != nil {
 		panic(err)
 	}
@@ -45,4 +85,12 @@ func StartHttpRouter() {
 func LogRequest(r *http.Request) {
 	requestID := fmt.Sprintf("%d", time.Now().UnixNano())
 	logger.Println(requestID, r.Method, r.URL.Path, r.RemoteAddr, r.UserAgent())
+}
+
+func ErrorResponse(w http.ResponseWriter, err string) {
+	json.NewEncoder(w).Encode(map[string]interface{}{"success": false, "error": err})
+}
+
+func SuccessResponse(w http.ResponseWriter, data interface{}) {
+	json.NewEncoder(w).Encode(map[string]interface{}{"success": true, "data": data})
 }
