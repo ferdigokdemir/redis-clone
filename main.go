46225b63babf1247348c6f2e0e0efbe1d16b4664
Ferdi <gokdemirferdi@gmail.com>
2021-10-16 16:23:51 +0300

    heroku deploy

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ var logger *log.Logger
 
 func init() {
 	// Set Environment Variables
-	os.Setenv("PORT", "8080")
+	os.Setenv("PORT", "80")
 	os.Setenv("DATABASE_SAVE_INTERVAL_SECONDS", "60")
 	os.Setenv("ENABLE_CORS", "true")
 	logger = log.New(os.Stdout, "http: ", log.LstdFlags)
